14d85954217f815d23687e36323b068c57288707
jaegyoung <worudkin@naver.com>
2022-04-21 09:35:08 +0900

    jsx 문법에서 html 사용시 주의점.

--- src/Address.js
+++ src/Address.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// jsx 문법에서는 html 태그를 사용 시 반드시 시작태그와 끝태그가 존재해야한다.
+// <img> -> <img></img>, <img /> , <br> -> <br></br>, <br/>
 function Address({data}){
     return  <h3>
         나의 친구는 {data}
@@ -7,3 +9,17 @@ function Address({data}){
 }
 
 export {Address};
+
+ /*props 를 이용한 객체 분할 방식 중 하나로 
+ function Addres(props{
+     const { name, gender, email} = props; 
+     이런식으로 상수 , 배열로 props 를 넣어서 따로 사용할려고 한다.
+
+     return (
+         <label > 이름 </label> {name}
+         이런식으로 사용 가능하며 얘를 받아주는 App에서 <div> 안에 <Address name = "" , gender ="" , email =""} 이런식으로 사용 가능하다.
+     )
+ }
+*/
+
+
